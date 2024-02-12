585f9a54e56bca9738c01b46e3acd912932b86d3
Krystyna90 <ukrbride4u@gmail.com>
2024-02-12 16:46:49 +0100

    component

--- intro2angular/src/app/app.component.ts
+++ intro2angular/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'intro2angular';
 }
